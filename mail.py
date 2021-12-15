a9509c35589021e7f91bdd5c2afcad4f91d02d6f
Krishna Pal <54345428+Krishna3-cloud@users.noreply.github.com>
2021-12-15 19:52:21 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -5,11 +5,11 @@ import smtplib
 server=smtplib.SMTP('smtp.gmail.com' , 587)
 server.starttls()
 
-server.login("<sender_mail_id>", "<password>")
+server.login("krishnapal3sep@gmail.com", "kraze92017warrior#159")
 subject = 'Jenkins_Task'
 
 msg = "Your code has been failed. Website is not loaded"
 
-server.sendmail("<sender_mail_id", "<receiver_mail_id>", msg)
+server.sendmail("krishnapal3sep@gmail.com", "soulstormerkraze19@gmail.com", msg)
 print("Email has been sent successfully !")
 server.quit()
