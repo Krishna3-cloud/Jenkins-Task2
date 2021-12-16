af3b61ed833cf1eba8876489d160faab8f57336a
Krishna Pal <54345428+Krishna3-cloud@users.noreply.github.com>
2021-12-16 12:27:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-print "<h1> Helloo World!! This is a Sample Testing Website to demonstrate this project.</h1><br>";
+print "<h1> Helloo World!! This is a Sample Testing Website to demonstrate this project. Thankyou!!!</h1><br>";
 ?>
